ae1d93c56b541c2d090a4546451143d77d1491ba
seula <seulaayoon@gmail.com>
2023-05-04 11:41:17 -0400

    [ADD] person input zero alert

--- tip-calculator-app/src/components/People.js
+++ tip-calculator-app/src/components/People.js
@@ -2,13 +2,37 @@ import React from "react"
 import '../styles/People.css';
 
 const People = ({updatePerson, reset}) => {
+
+    const input = React.useRef();
+    const alert = React.useRef();
     const [value, setValue] = React.useState('');
 
     function handleInputChange(e) {
         const userValue = e.target.value;
-        updatePerson(userValue);
         setValue(userValue);
+            
+    
+        updatePerson(userValue);
+        
     }
+    function onAlertMode() {
+        input.current.classList.add("zero");
+        alert.current.classList.remove("off");
+    }
+    function offAlertMode() {
+        input.current.classList.remove("zero");
+        alert.current.classList.add("off");
+
+    }
+    React.useEffect(() => {
+        console.log(value)
+        if(value == "0") {
+            onAlertMode();
+        } else {
+            offAlertMode();
+        }
+  
+    }, [value])
     React.useEffect(()=> {
         if(reset) {
          setValue('')
@@ -17,8 +41,10 @@ const People = ({updatePerson, reset}) => {
  
     return (
         <div className="people">
-            <div className="people-title">Number of People</div>
-            <input className="people-input" type="text" value={value} onChange={handleInputChange}></input>
+            <div className="people-title">Number of People
+            <div className="people-alert off" ref={alert}>Can't be zero</div>
+                </div>
+            <input className="people-input" type="text" value={value} onChange={handleInputChange} ref={input}></input>
         </div>
     )
 }
